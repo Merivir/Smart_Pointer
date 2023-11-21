db1ac7a46b1b354ab758b8daeff0609b4e23f711
Merivir <118295002+Merivir@users.noreply.github.com>
2023-11-21 21:50:25 +0400

    Create shared.h

---
+++ shared.h
@@ -0,0 +1,27 @@
+#ifndef SHARED_H
+#define SHARED_H
+
+#include "controlBlock.hpp"
+
+template <typename T>
+class Weak;
+
+template <typename T>
+class Shared {
+public:
+    Shared(T* ptr);
+    Shared(const Shared& other);
+    Shared(const Weak<T>& weak);
+    ~Shared();
+    Shared& operator=(const Shared& other);
+    T& operator*();
+    template <typename U = T, typename = std::enable_if_t<!std::is_fundamental_v<U>>>
+    T* operator->();
+    int refCount() const;
+
+private:
+    ControlBlock<T>* m_ptr;
+    friend class Weak<T>;
+};
+
+#endif // SHARED_H
