7a444424336254c090702e6e9a6697f15fe78a54
Merivir <118295002+Merivir@users.noreply.github.com>
2023-11-21 21:51:37 +0400

    Create shared.cpp

---
+++ shared.cpp
@@ -0,0 +1,74 @@
+#include "shared.h"
+
+template <typename T>
+Shared<T>::Shared(T* ptr) : m_ptr(new ControlBlock<T>(ptr)) {}
+
+template <typename T>
+Shared<T>::Shared(const Shared& other) : m_ptr(other.m_ptr) {
+    if (m_ptr) {
+        m_ptr->refCount++;
+    }
+}
+
+template <typename T>
+Shared<T>::Shared(const Weak<T>& weak) : m_ptr(weak.m_pt) {
+    if (m_ptr) {
+        m_ptr->refCount++;
+    }
+}
+
+template <typename T>
+Shared<T>::~Shared() {
+    if (m_ptr) {
+        m_ptr->refCount--;
+    }
+    if (m_ptr->refCount == 0) {
+        delete m_ptr;
+    }
+}
+
+template <typename T>
+Shared<T>& Shared<T>::operator=(const Shared& other) {
+    if (this != &other) {
+        if (m_ptr) {
+            m_ptr->refCount--;
+        }
+        if (m_ptr->refCount == 0) {
+            delete m_ptr;
+        }
+
+        m_ptr = other.m_ptr;
+        m_ptr->refCount = other.m_ptr->refCount;
+
+        if (m_ptr) {
+            m_ptr->refCount++;
+        }
+    }
+
+    return *this;
+}
+
+template <typename T>
+T& Shared<T>::operator*() {
+    if (m_ptr == nullptr) {
+        throw std::runtime_error("You are dereferencing nullptr");
+    }
+    return *(m_ptr->m_val);
+}
+
+template <typename T>
+template <typename U, typename>
+T* Shared<T>::operator->() {
+    if (m_ptr == nullptr) {
+        throw std::runtime_error("You are dereferencing nullptr");
+    }
+    return m_ptr->m_val;
+}
+
+template <typename T>
+int Shared<T>::refCount() const {
+    if (m_ptr == nullptr) {
+        throw std::runtime_error("You are dereferencing nullptr");
+    }
+    return m_ptr->refCount;
+}
